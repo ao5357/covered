81c4979662b2e58909cb92f188b3abef73771963
Brad Czerniak <ao5357@gmail.com>
2012-04-10 19:19:32 -0400

    Tab indents And the variable declarations that IE seems to hate

--- javascripts/covered.js
+++ javascripts/covered.js
@@ -1,14 +1,15 @@
 $.extend({
-  constructDoc: function(d,facets){
-    var $node = $('<div />').attr({class: 'doc ' + d.data_source, id: 'doc-' + d.id}), nodeContent = '';
-    nodeContent += (d.id_isbn)? '<img src="http://covers.openlibrary.org/b/isbn/' + d.id_isbn[0] + '-S.jpg" class="cover" />' : '';
-    if(d.content_link){nodeContent += '<a target="_blank" href="' + d.content_link[0] + '">';}
-    nodeContent += (d.title) ? $.ellipsisSubstr(d.title) : 'Untitled Work';
-    if(d.content_link){nodeContent += '</a>';}
-    nodeContent += '<span class="data_source">' + d.data_source + '</span>';
-    facets[d.data_source] = (facets[d.data_source] == undefined) ? 1 : (facets[d.data_source] + 1);
-    return $node.append(nodeContent).data('doc',d);
-  	},
+	constructDoc: function(d,facets){
+		var $node = $('<div />').attr({class: 'doc ' + d.data_source, id: 'doc-' + d.id});
+		var nodeContent = '';
+		nodeContent += (d.id_isbn)? '<img src="http://covers.openlibrary.org/b/isbn/' + d.id_isbn[0] + '-S.jpg" class="cover" />' : '';
+		if(d.content_link){nodeContent += '<a target="_blank" href="' + d.content_link[0] + '">';}
+		nodeContent += (d.title) ? $.ellipsisSubstr(d.title) : 'Untitled Work';
+		if(d.content_link){nodeContent += '</a>';}
+		nodeContent += '<span class="data_source">' + d.data_source + '</span>';
+		facets[d.data_source] = (facets[d.data_source] == undefined) ? 1 : (facets[d.data_source] + 1);
+		return $node.append(nodeContent).data('doc',d);
+	},
 
 	ellipsisSubstr: function(inString){
 		var max = arguments[1] || 100, suffix = arguments[2] || '…';
@@ -60,77 +61,77 @@ $(document).ready(function(){
 	/* Remove filters */
 	$form.on('click','sup',function(){
 		$(this).parent().remove();
-  	});
-  
-  /* Add filter when toggle is clicked
-   * Called by form submit as well
-   */
+	});
+
+	/* Add filter when toggle is clicked
+	 * Called by form submit as well
+	 */
 	$('#add_searchable').on('click',function(){
 		var $term = $form.find("#term").focus(), $searchable_select = $form.find('#searchable_select');
 		if($term.val().length >= 1){
 			$form.find('#terms').append('<span class="searchable_term" data_term_value="' + $term.val() + '" data_term_searchable="' +$searchable_select.val() + '"><span class="term_field">' + $searchable_select.val() + '</span>: ' + $term.val() + '<sup> X </sup>');
-      $form[0].reset();
-      $form.find('#start').val(0);
-    	}
-    return false;  
+		$form[0].reset();
+		$form.find('#start').val(0);
+		}
+	return false;  
 		});
-	
+
 	/* Limit results to the facets */
 	$("#facets").on('click','.filter',function(){
-    $target.isotope({filter: $(this).attr('data_filter_class')});
-    });
-	
+	$target.isotope({filter: $(this).attr('data_filter_class')});
+	});
+
 	/* When form is submitted, get results */
-  $form.on('submit',function(){
-    $form.find("#add_searchable").click();
-    $form.find('#submit').val('please wait…');
-    $form.find('#meta').empty();
-    $target.empty().isotope('destroy');
-    $('#messages').empty();
-    $('#facets').empty();
-    
-    var query = [];
+	$form.on('submit',function(){
+		$form.find("#add_searchable").click();
+		$form.find('#submit').val('please wait…');
+		$form.find('#meta').empty();
+		$target.empty().isotope('destroy');
+		$('#messages').empty();
+		$('#facets').empty();
+
+	var query = [];
 			$('.searchable_term').each(function(){
 				query.push({name: 'filter', value: $(this).attr('data_term_searchable') + ':' + $(this).attr('data_term_value')});
 				});
-    	if(query.length == 0){ // If there are no filters, don't do anything else
-    		$('#messages').append('Please enter a query term and click "add term".');
-    		return false;
-    		}
-    	query.push({name: "start", value: $form.find("#start").val()});
+		if(query.length == 0){ // If there are no filters, don't do anything else
+			$('#messages').append('Please enter a query term and click "add term".');
+			return false;
+			}
+		query.push({name: "start", value: $form.find("#start").val()});
 
-    $.getJSON('http://api.dp.la/dev/item?' + $.param(query) + '&callback=?')
-    	.done(function(data){
-    		$target.isotope({columnWidth: 300,itemSelector : '.doc',layoutMode : 'masonry'});
-    		var facets = {};
-        if(data.docs){
-        	var start = +data.start, limit = +data.limit, num_found = +data.num_found, metaMsg = '';
-        	$.each(data.docs,function(i,record){
-    				$target.isotope('insert',$.constructDoc(record,facets));
-    				});
-    			$.each(facets,function(key,val){
-          	$('#facets').append('<span class="filter" data_filter_class=".' + key + '">' + key + ' - ' + val + '</span>');
-        		});
-        	$('#facets').append('<span class="filter" data_filter_class="*">Show all</span>');
-        	metaMsg += (start >= 1) ? '<span class="paginate" data_pagination_start="' + (start - limit) + '">&laquo; Previous</span>' : '';
-        	metaMsg += (start + 1) + ' to ';
-        	metaMsg += (num_found < (start + limit)) ? num_found : (start + limit);
-        	metaMsg += ' of ' + num_found + ' found';
-        	metaMsg += ((start + limit) < num_found) ? '<span class="paginate" data_pagination_start="' + (start + limit) + '">Next &raquo;</span>' : '';
-        	$("#meta").append(metaMsg);
-        	}
-        else{
-        	$('#messages').html('None found.');
-        	}
-    		})
-    	.fail(function(){console.log("GLARING ERROR");})
-    	.always(function(){
-    		$('#submit').val('go!');
-    		});
-  	return false;
-  	});
-  
-  /* Make AJAX calls to services when a result is clicked */
+	$.getJSON('http://api.dp.la/dev/item?' + $.param(query) + '&callback=?')
+		.done(function(data){
+			$target.isotope({columnWidth: 300,itemSelector : '.doc',layoutMode : 'masonry'});
+			var facets = {};
+		if(data.docs){
+			var start = +data.start, limit = +data.limit, num_found = +data.num_found, metaMsg = '';
+			$.each(data.docs,function(i,record){
+					$target.isotope('insert',$.constructDoc(record,facets));
+					});
+				$.each(facets,function(key,val){
+		  	$('#facets').append('<span class="filter" data_filter_class=".' + key + '">' + key + ' - ' + val + '</span>');
+				});
+			$('#facets').append('<span class="filter" data_filter_class="*">Show all</span>');
+			metaMsg += (start >= 1) ? '<span class="paginate" data_pagination_start="' + (start - limit) + '">&laquo; Previous</span>' : '';
+			metaMsg += (start + 1) + ' to ';
+			metaMsg += (num_found < (start + limit)) ? num_found : (start + limit);
+			metaMsg += ' of ' + num_found + ' found';
+			metaMsg += ((start + limit) < num_found) ? '<span class="paginate" data_pagination_start="' + (start + limit) + '">Next &raquo;</span>' : '';
+			$("#meta").append(metaMsg);
+			}
+		else{
+			$('#messages').html('None found.');
+			}
+			})
+		.fail(function(){console.log("GLARING ERROR");})
+		.always(function(){
+			$('#submit').val('go!');
+			});
+	return false;
+	});
+
+	/* Make AJAX calls to services when a result is clicked */
 	$target.on('click','.doc',function(){
 		$(this).xISBNjacketsLT().subjectFlickr().delay(2500).queue(function(){
 			$('.isotope').isotope('reLayout');
